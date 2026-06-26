feda341b930eb1f96cc5a02351f85cf5cab4ff29
evgenydef <evgenyzv2005@gmail.com>
2026-06-26 17:58:52 +0400

    Errors messages for registration and login pages

--- surface-network/src/app/login-component/login-component.ts
+++ surface-network/src/app/login-component/login-component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 
+import { FormsModule, NgModel } from '@angular/forms';
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-component.html',
   styleUrl: './login-component.css',
 })
 export class LoginComponent {
+  nickname: string = '';
+  password: string = '';
 
+  get isNicknameValid(): boolean {
+    return this.nickname.length >= 3 && this.nickname.length <= 20
+  }
+
+  get isValidLength(): boolean {
+    return this.password.length >= 6 && this.password.length <= 20;
+  }
+
+  get isValidSymbols(): boolean {
+    const regex = /^[\w+]+$/
+    return regex.test(this.password);
+  }
 }
--- surface-network/src/app/register-component/register-component.ts
+++ surface-network/src/app/register-component/register-component.ts
@@ -1,11 +1,87 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-register-component',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register-component.html',
   styleUrl: './register-component.css',
 })
 export class RegisterComponent {
 
+  //  поле для ввода "Псевдонима"
+  nickname: string = '';
+
+  // Поле для ввода пароля
+  password: string = '';
+
+  email: string = ''
+
+  // Поле для повторного ввода пароля
+  repeatedPassword: string = '';
+
+  // Поле для ввода фамилии
+  lastname: string = '';
+
+  // Поле для ввода имени
+  firstname: string = '';
+
+  // Поле для ввода фотографии
+  photo: string = '';
+
+  // Поле для ввода контактов
+  contacts: string = '';
+
+  // Поля для ввобда информации о себе
+  aboutMyself: string = '';
+
+  // Поле для ввода достижений
+  achievements: string = '';
+
+  equalsPasswords(password: string, repeatedPassword: string): boolean {
+    return password === repeatedPassword;
+  }
+
+  get isNicknameValid(): boolean {
+    return this.nickname.length >= 3 && this.nickname.length <= 20
+  }
+
+  get isEmailLengthValid(): boolean {
+    return this.email.length <= 31;
+  }
+
+  get isEmailValid(): boolean {
+    return this.email.includes('@');
+  }
+
+  get isValidLength(): boolean {
+    return this.password.length >= 6 && this.password.length <= 20;
+  }
+
+  get isValidSymbols(): boolean {
+    const regex = /^[\w+]+$/
+    return regex.test(this.password);
+  }
+
+  get isFirstnameValid(): boolean {
+    return this.firstname.length <= 31;
+  }
+
+  get isLastnameValid(): boolean {
+    return this.lastname.length <= 31;
+  }
+
+  get isContactsValid(): boolean {
+    return this.contacts.length <= 255;
+  }
+
+    get isAboutMyselfValid(): boolean {
+    return this.contacts.length <= 255;
+  }
+
+    get isAchievementsValid(): boolean {
+    return this.contacts.length <= 255;
+  }
 }
